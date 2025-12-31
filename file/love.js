2cab61ec06efb43dffdab3afe02c0f85828d86e4
Swastik2399 <vermaswastik58@gmail.com>
2025-12-31 22:39:35 +0530

    Update love.js

--- file/love.js
+++ file/love.js
@@ -702,7 +702,7 @@
             ctx.scale(0.75, 0.75);
             ctx.font = "12px,Verdana"; // 字号肿么没有用? (ˉ(∞)ˉ)
             ctx.fillText("Click Me:) ", 30, -5);
-            ctx.fillText("Birthday Queen !", 28, 10);
+            ctx.fillText(" BABY GIRL!", 28, 10);
             ctx.restore();
         },
         clear: function() {
@@ -1065,3 +1065,4 @@
 
 
 })(window);
+
